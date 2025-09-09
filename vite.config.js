4dabd5fd5d797f9c666961c87a48c1e48a16d80e
anac2828 <anac2828@yahoo.com>
2025-09-09 15:02:15 -0700

    added %BASE_URL% to logo link

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import htmlPurge from 'vite-plugin-purgecss';
 
 export default defineConfig({
   plugins: [htmlPurge()],
-  build: { outDir: 'dist' },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        about: '/pages/how-we-work.html',
+        pricing: '/pages/pricing.html',
+      },
+    },
+  },
   base: '/brandboosters',
 });
